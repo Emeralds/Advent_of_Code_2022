5fc89054bf30b20d71e52b1d39f6092e86203550
Benjamin King <bk.emeralds@gmail.com>
2022-12-12 15:56:57 -0500

    Base files set up

--- Day 1/Day1_1.py
+++ Day 1/Day1_1.py
@@ -17,7 +17,7 @@
 
 ## Find the Elf carrying the most Calories. How many total Calories is that Elf carrying? In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
 
-text_file = open("Day 1\Day1_1_Input.txt", "r")
+text_file = open("Day 1\Day1_Input.txt", "r")
 lines = text_file.readlines()
 text_file.close()
 
@@ -30,4 +30,9 @@ for x in lines:
     else:
         a = a + int(x)
 calories.sort()
-print("Highest sum of calories is:", calories[-1])
+
+# Answer Part 1
+print("Part 1 - Highest sum of calories is:", calories[-1])
+
+# Answer Part 2
+print("Part 2 - Sum of top 3 elves calories is:", (calories[-1]+calories[-2]+calories[-3]))
